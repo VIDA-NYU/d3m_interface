cca3c3c769c229e6879d0abd334e26d404b16759
Roque Lopez <rlopezc27@gmail.com>
2020-07-21 16:55:34 -0400

    Update dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="d3m_interface",
-    version="0.1.9",
+    version="0.1.10",
     author="Roque Lopez",
     author_email="rlopez@nyu.edu",
     description="Library to use D3M AutoML Systems",
