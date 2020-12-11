1eb1869449349f3adf654e7496733e99233a9a03
Roque Lopez <rlopezc27@gmail.com>
2020-12-11 16:27:01 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="d3m_interface",
-    version="0.1.24",
-    author="Roque Lopez",
-    author_email="rlopez@nyu.edu",
-    description="Library to use D3M AutoML Systems",
+    name='d3m_interface',
+    version='0.1.24',
+    author='Roque Lopez',
+    author_email='rlopez@nyu.edu',
+    description='Library to use D3M AutoML Systems',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://gitlab.com/ViDA-NYU/d3m/d3m_interface",
+    long_description_content_type='text/markdown',
+    url='https://gitlab.com/ViDA-NYU/d3m/d3m_interface',
     packages=setuptools.find_packages(),
     license='Apache-2.0',
     classifiers=[
-        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3',
         'License :: OSI Approved :: Apache Software License',
-        "Operating System :: OS Independent",
+        'Operating System :: OS Independent',
     ],
     install_requires=[
         'd3m==2020.11.3',
