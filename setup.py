e0ce6b9dfcb9a6bed81492765b6de332ca04928d
Roque Lopez <rlopezc27@gmail.com>
2022-02-11 16:08:08 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='d3m_interface',
-    version='0.4.1.dev0',
+    version='0.4.1',
     author='Roque Lopez, Remi Rampin, Sonia Castelo',
     author_email='rlopez@nyu.edu, remi.rampin@nyu.edu, s.castelo@nyu.edu',
     description='Library to use D3M AutoML Systems',
@@ -20,8 +20,8 @@ setuptools.setup(
         'Operating System :: OS Independent',
     ],
     install_requires=[
-        'd3m-automl-rpc @ git+https://gitlab.com/datadrivendiscovery/automl-rpc.git@dev-dist-python',
-        'd3m @ git+https://gitlab.com/datadrivendiscovery/d3m.git@devel#egg=d3m',
+        'd3m==2021.12.19',
+        'd3m-automl-rpc==1.2.0',
         'pandas>=1.1.3,<=1.3.4',
         'numpy',
         'scikit-learn',
